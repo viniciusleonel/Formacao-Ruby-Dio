5c81af222422fd82ba056f49da7b43119909efe9
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-11-14 18:32:13 -0300

    primeiro desafio formacao ruby dio

---
+++ Desafio1/desafio1.rb
@@ -0,0 +1,8 @@
+puts "Digite seu nome: "
+nome = gets.chomp.capitalize
+puts "Digite seu sobrenome: "
+sobrenome = gets.chomp.capitalize
+puts "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "Seu nome é #{nome} #{sobrenome} e você tem #{idade} anos!"
