97faeb6cbce8e7f2f34ff8856e4639f6b049bace
vinicius-leonel <viniciuslps.cms@gmail.com>
2024-11-14 21:09:25 -0300

    desafio calculadora

---
+++ Desafio2/calculadora.rb
@@ -0,0 +1,22 @@
+puts "Abrindo Calculadora"
+puts "Digite o primeiro número: "
+num1 = gets.chomp.to_f
+puts "Digite o segundo número: "
+num2 = gets.chomp.to_f
+
+puts "Qual função da calculadora deseja usar?"
+puts "Multiplicar, Dividir, Somar, Subtrair"
+escolha = gets.chomp.downcase
+
+case escolha
+when "somar"
+  puts "#{num1} + #{num2} = #{num1+num2}"
+when "multiplicar"
+  puts "#{num1} * #{num2} = #{num1*num2}"
+when "subtrair"
+  puts "#{num1} - #{num2} = #{num1-num2}"
+when "dividir"
+  puts "#{num1} / #{num2} = #{num1/num2}"
+  else
+  puts "Função inválida"
+end
